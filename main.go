6bc488360c347d0ffbb81e6935e1bc513e3adca5
Arnaud Wanet <andrewwanet9@gmail.com>
2023-08-07 13:02:17 +0100

    removed the logic inside the download function for further improvments

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
+	//"net/http"
 	"os"
-	"strings"
+	//"strings"
 
-	"github.com/PuerkitoBio/goquery"
-	"github.com/schollz/progressbar/v3"
+	//"github.com/PuerkitoBio/goquery"
+	//"github.com/schollz/progressbar/v3"
 	"github.com/urfave/cli/v2"
 )
 
@@ -17,9 +17,12 @@ func main(){
 		Name: "spotigo",
 		Usage: "Download songs from Youtube and Spotify",
 		Action: func(c *cli.Context) error{
-			song := c.Args().Get(0)
-			artist := c.Args().Get(1)
-			downloadSong(song, artist)
+			if c.Args().Get(0) != "" && c.Args().Get(1) != ""{
+				song := c.Args().Get(0)
+				artist := c.Args().Get(1)
+				downloadSong(song, artist)
+				return nil
+			}
 			return nil
 		},
 		ArgsUsage: "<song> <artist>",
@@ -33,72 +36,6 @@ func main(){
 
 
 func downloadSong(song, artist string){
-	query := fmt.Sprintf("%s %s official audio", song, artist)
-
-	// Search for the song on Youtube
-	resp, err := http.Get("https://www.youtube.com/results?search_query="+ query)
-	if err != nil{
-		log.Fatalf("Failed to search Youtube: %v", err)
-	}
-	defer resp.Body.Close()
-
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil{
-		log.Fatalf("Failed to parse search results: %v", err)
-	}
-
-	// Find the first Youtube video link
-	videoLink := doc.Find("a.yt-uix-tile-link").First()
-	if videoLink.Length() == 0{
-		log.Fatalf("Video not found for: %s by %s", song, artist)
-	}
-
-	videoURL, _ := videoLink.Attr("href")
-	videoURL = "https://www.youtube.com" + videoURL
-
-	// Download the audio stream
-	resp, err = http.Get(videoURL)
-	if err != nil{
-		log.Fatalf("Failed to retrieve Youtube video details: %v", err)
-	}
-
-	defer resp.Body.Close()
-
-	doc, err = goquery.NewDocumentFromReader(resp.Body)
-	if err != nil{
-		log.Fatalf("Failed to parse Youtube video details: %v", err)
-	}
-
-	audioURL, _ := doc.Find("script:contains('audioDetails')").First().Html()
-	audioURL = strings.Split(strings.Split(audioURL, `"audioDetails":{"adapttiveFormats":[{"url":`))[0]
-
-	// Download the audio file
-	resp, err := http.Get(audioURL)
-	if err != nil{
-		log.Fatalf("Failed to download the audio file: %v", err)
-	}
-
-	defer resp.Body.Close()
-
-	file, err := os.Create(fmt.Sprintf("%s - %s.mp3", song, artist))
-
-	if err != nil{
-		log.Fatalf("Failed to create audio file: %v", err)
-	}
-
-	defer file.Close()
-
-	bar := progressbar.DefaultBytes(
-		resp.ContentLength,
-		"Downloading"
-	)
-
-	bar.RenderBlank()
-
-	_, err = bar.Copy(resp.Body, file)
-	if err != nil{
-		log.Fatalf("Failed to write audio file: %v", err)
-	}
-
-	fmt.Println("Download Complete")
+	fmt.Printf("Downloading song %s by %s", song, artist)
+	fmt.Println("\nDownload Complete")
 }
