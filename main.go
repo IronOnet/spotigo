a08867f358d35768c093bfe4f6af49aadc34ab7e
Arnaud Wanet <andrewwanet9@gmail.com>
2023-08-09 08:53:37 +0100

    added video download logic

--- main.go
+++ main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
-	//"net/http"
+	"net/http"
 	"os"
+
 	//"strings"
 
-	//"github.com/PuerkitoBio/goquery"
-	//"github.com/schollz/progressbar/v3"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/schollz/progressbar/v3"
 	"github.com/urfave/cli/v2"
 )
 
-func main(){
+func main() {
 	app := &cli.App{
-		Name: "spotigo",
+		Name:  "spotigo",
 		Usage: "Download songs from Youtube and Spotify",
-		Action: func(c *cli.Context) error{
-			if c.Args().Get(0) != "" && c.Args().Get(1) != ""{
+		Action: func(c *cli.Context) error {
+			if c.Args().Get(0) != "" && c.Args().Get(1) != "" {
 				song := c.Args().Get(0)
 				artist := c.Args().Get(1)
 				downloadSong(song, artist)
@@ -29,13 +31,82 @@ func main(){
 	}
 
 	err := app.Run(os.Args)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func downloadSong(song, artist string) {
+	fmt.Printf("\nDownloading song %s by %s", song, artist)
 
-func downloadSong(song, artist string){
-	fmt.Printf("Downloading song %s by %s", song, artist)
+	// The algorithm
+	// Take two string inputs from the Arg
+	// Pass those string inputs to a search query
+	// the search query returns a list of results
+	// select the first video returned
+	// download the audio stream from the video
+	// selected
+	// save the audio file in a folder specified in the --path argument
+
+	query := fmt.Sprintf("%s+%s+official+audio", song, artist)
+
+	// Search for the song on Youtube
+	resp, err := http.Get("https://www.youtube.com/results?search_query=" + query)
+	if err != nil {
+		log.Fatalf("Failed to search Youtube: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body) 
+	if err != nil{
+		log.Fatalf("\nfailed to parse search results: %v", err)
+	}
+
+	// Find the first Youtube video Link 
+	videoLink := doc.Find("a.yt-uix-tile-link").First() 
+	if videoLink.Length() == 0{
+		log.Fatalf("\nvideo not found for: %s by %s", song, artist)
+	} else{
+		fmt.Println("\nVideo found")
+	}
+
+	videoUrl, _ := videoLink.Attr("href") 
+	videoUrl = "https://www.youtube.com" + videoUrl 
+
+	// Download the audio stream 
+	resp, err = http.Get(videoUrl) 
+	if err != nil{
+		log.Fatalf("\nFailed to retrieve the youtube video details: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	file, err := os.Create(fmt.Sprintf("%s - %s.mp3", song, artist))
+	if err != nil{
+		log.Fatalf("Failed to create audio file: %v", err)
+	}
+	defer file.Close()
+
+	bar := progressbar.DefaultBytes(
+		resp.ContentLength, 
+		"Downloading",
+	)
+
+	bar.RenderBlank()
+
+	_, err = io.Copy(io.MultiWriter(file, bar), resp.Body)
+	if err != nil{
+		log.Fatalf("failed to write audio file: %v", err)
+	}
+
+
+
+	fmt.Println(string(bodyBytes))
 	fmt.Println("\nDownload Complete")
 }
